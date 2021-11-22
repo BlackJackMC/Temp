6e45a7802471ccb3c7ce5b0a0123f497245053c4
BlackJackMC <68107237+BlackJackMC@users.noreply.github.com>
2021-11-22 22:06:37 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,43 +1,140 @@
 #include <iostream>
 using namespace std;
-string multiply(string first, string second)
+string b1, b2;
+string hexa[16][2] =
+    {
+        {"0000", "0"},
+        {"0001", "1"},
+        {"0010", "2"},
+        {"0011", "3"},
+        {"0100", "4"},
+        {"0101", "5"},
+        {"0110", "6"},
+        {"0111", "7"},
+        {"1000", "8"},
+        {"1001", "9"},
+        {"1010", "A"},
+        {"1011", "B"},
+        {"1100", "C"},
+        {"1101", "D"},
+        {"1110", "E"},
+        {"1111", "F"}};
+string octa[8][2] =
+    {
+        {"000", "0"},
+        {"001", "1"},
+        {"010", "2"},
+        {"011", "3"},
+        {"100", "4"},
+        {"101", "5"},
+        {"110", "6"},
+        {"111", "7"}};
+void compact(string &number)
 {
-    int temp;
-    int carry=0;
+    while (number[0] == '0')
+    {
+        number.erase(0, 1);
+        if (number.empty())
+        {
+            number == "0";
+            return;
+        }
+    }
+    return;
+}
+void balance(string &number, int length)
+{
+    while (number.length() < length)
+    {
+        number.insert(0, "0");
+    }
+    return;
+}
+string convert(string n, int base)
+{
     string ans;
-    for(int i=first.length()-1; i>=0;i--)
+    if (base == 16)
     {
-        temp=(first[i]-'0') * stoi(second)+carry;
-        carry=temp/10;
-        temp%=10;
-        ans=char(temp+'0')+ans;
+        if (n.length() % 4 != 0)
+        {
+            int temp = n.length() % 4;
+            for (int i = 1; i <= 4 - temp; i++)
+            {
+                n = "0" + n;
+            }
+        }
+        while (n.length() != 0)
+        {
+            string temp;
+            temp = n.substr(n.length() - 4, 4);
+            n.erase(n.length() - 4, 4);
+            for (int i = 0; i < 16; i++)
+            {
+                if (hexa[i][0] == temp)
+                {
+                    ans = hexa[i][1] + ans;
+                }
+            }
+        }
+        return ans;
     }
-    if(carry!=0)
+    else
     {
-        ans=to_string(carry)+ans;
+        while (n.length() != 0)
+        {
+            string temp;
+            if (n.length() % 3 != 0)
+            {
+                n = "0" + n;
+            }
+            temp = n.substr(n.length() - 3, 3);
+            n.erase(n.length() - 3, 3);
+            for (int i = 0; i < 8; i++)
+            {
+                if (octa[i][0] == temp)
+                {
+                    ans = octa[i][1] + ans;
+                }
+            }
+        }
+        return ans;
     }
+}
+string add()
+{
+    string ans;
+    int carry = 0;
+    int temp;
+    for (int i = b1.length() - 1; i >= 0; i--)
+    {
+        temp = (b1[i] - '0') + (b2[i] - '0') + carry;
+        ans = char((temp % 2) + '0') + ans;
+        carry = temp / 2;
+    }
+    if (carry)
+    {
+        ans = "1" + ans;
+    }
+    compact(ans);
     return ans;
 }
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n;
-    cin>>n;
-    if(n==1 or n==0)
+    string result;
+    cin >> b1 >> b2;
+    if (b1.length() > b2.length())
     {
-        cout<<1;
-        return 0;
+        balance(b2, b1.length());
     }
     else
     {
-        string current="1";
-        string temp;
-        for(int i=2;i<=n;i++)
-        {
-            temp=to_string(i);
-            current= multiply(current,temp);
-        }
-        cout<<current;
+        balance(b1, b2.length());
     }
+    result = add();
+    cout << result << "\n\n"
+         << convert(result, 8) << "\n\n"
+         << convert(result, 16);
+    return 0;
 }
